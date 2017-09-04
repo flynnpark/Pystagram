24c7371831e87826c1df5000013043523fbffe42
wphestiraid <wphestiraid@gmail.com>
2017-09-04 20:22:56 +0900

    media 파일 서빙을 위한 설정 및 urlpatterns 추가

--- pystagram/urls.py
+++ pystagram/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.shortcuts import redirect
@@ -22,3 +24,6 @@ urlpatterns = [
     url(r'^accounts/', include('accounts.urls')),
     url(r'^$', lambda r: redirect('profile'), name='root'), # NOTE : lambda 함수를 통한 리다이렉트
 ]
+
+# NOTE: serving media files uploaded by user during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
