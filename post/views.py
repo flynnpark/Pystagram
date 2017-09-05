b1f7bdefb42c66d116cafb8a3b90f14bafcd1fca
wphestiraid <wphestiraid@gmail.com>
2017-09-05 11:57:51 +0900

    post_list 뷰 구현

--- post/views.py
+++ post/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    post_list = Post.objecs.all()
+    return render(request, 'post/post_list.html', {
+        'post_list': post_list,
+    })
