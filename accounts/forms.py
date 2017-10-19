d76884bd5bbc8b8045c7934a5d4c7bd6b5702c64
Flynn <wphestiraid@gmail.com>
2017-10-19 15:23:29 +0900

    Profile 모델에 about, gender 필드 추가 - 회원정보 수정시 해당 항목 함께 노출

--- accounts/forms.py
+++ accounts/forms.py
@@ -38,9 +38,14 @@ class SignupForm(UserCreationForm):
         return user
 
 class ProfileForm(forms.ModelForm):
+    about = forms.CharField(widget = forms.Textarea(attrs = {
+        'rows': 4,
+        'cols': 50,
+        'placeholder': '소개는 150자까지 등록 가능합니다',
+    }))
     class Meta:
         model = Profile
-        fields = ['nickname', 'picture']
+        fields = ['nickname', 'picture', 'about', 'gender']
 
     def clean_picture(self):
         picture = self.cleaned_.get('picture')
