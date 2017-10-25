1f1f1df648291d0fdeda51835d0cb7e6bd57f6d1
Flynn <wphestiraid@gmail.com>
2017-10-25 22:46:40 +0900

    static 파일 관련 설정 추가, 부트스트랩 적용

--- pystagram/settings.py
+++ pystagram/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'pystagram', 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (uploaded by user)
 MEDIA_URL = '/media/'
